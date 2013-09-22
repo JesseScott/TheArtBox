480f5328120ab367268bd2c8815dc8821a3985df
Jesse Scott <jesse@memelab.ca>
2013-09-22 00:10:56 -0700

    Reorganizing Header File

--- TheArtBox/src/testApp.h
+++ TheArtBox/src/testApp.h
@@ -15,147 +15,86 @@
 class testApp : public ofBaseApp {
 public:
 	
+	// BASE METHODS
 	void setup();
 	void update();
 	void draw();
 	void exit();
-	
-	void drawPointCloud();
-	
 	void keyPressed(int key);
 	void mouseDragged(int x, int y, int button);
 	void mousePressed(int x, int y, int button);
 	void mouseReleased(int x, int y, int button);
 	void windowResized(int w, int h);
 	
+	// CUSTOM METHODS
+	void loadFonts();
+	void loadArtists();
+    void setAssets(int index);
+	void loadAssets();
+	void setColourThumbnailImage(int _width, int _height);
+	void setBlackAndWhiteThumbnailImage(ofImage img);
+	void updateCurrentIndex();
+    void setupGL(int _width, int _height);
+    int getCurrentBrightness();
+    void checkMemory();
+    void getFilesFromFTP();
+	void drawPointCloud();
+	
+	// KINECT & OPENCV	
 	ofxKinect kinect;
-	
 	ofxCvColorImage colorImg;
-	
-	ofxCvGrayscaleImage grayImage; // grayscale depth image
-	ofxCvGrayscaleImage grayThreshNear; // the near thresholded image
-	ofxCvGrayscaleImage grayThreshFar; // the far thresholded image
+	ofxCvGrayscaleImage grayImage;
+	ofxCvGrayscaleImage grayThreshNear;
+	ofxCvGrayscaleImage grayThreshFar; 
 	ofxCvGrayscaleImage grayBg;
 	ofxCvGrayscaleImage grayDiff;
-
 	ofxCvContourFinder contourFinder;
-
 	ofFbo closePoints; 
-
 	int minBlob; 
 	int maxBlob; 
-	
 	bool bThreshWithOpenCV;
 	bool bDrawPointCloud;
 	bool tooSunny;
 	int  threshold;
 	bool bLearnBakground;
-	
 	int nearThreshold;
 	int farThreshold;
-	
 	int angle;
-
-	ofImage stamp; 
-	
-	// used for viewing the point cloud
 	ofEasyCam easyCam;
 
-	// Added Methods
+    // Canvas
+	ofImage foreground;
+	ofImage thumbnail;
+	ofVideoPlayer demo;
+	ofVideoPlayer video;
+	ofImage image;
+	int imageTimer; 
+	int playState;
+	int width, height;
+	int maxIndex;
+	int currentIndex;
+	bool currentAssetIsMovie;
+	bool presenting;
+	bool showDemo;
 
-		void loadFonts();
+	// GL / GPU
+	ofShader shader;
+	ofFbo maskFbo;
+	ofFbo fbo;
+	int currentBrightness;
+	int fboAge; 
+	int targetAlpha;
+	ofImage stamp; 
 
-		void loadArtists();
+	// Painting
+	ofImage brush;
+	bool bBrushDown;
 
-        void setAssets(int index);
-
-		void loadAssets();
-
-
-
-		void setColourThumbnailImage(int _width, int _height);
-
-		void setBlackAndWhiteThumbnailImage(ofImage img);
-
-		void updateCurrentIndex();
-
-
-
-        void setupGL(int _width, int _height);
-
-        int getCurrentBrightness();
-
-
-
-        void checkMemory();
-
-        void getFilesFromFTP();
-
-
-
-        // Canvas
-
-        ofImage foreground;
-
-        ofImage thumbnail;
-
-		ofVideoPlayer demo;
-
-        ofVideoPlayer video;
-
-        ofImage image;
-
-		int imageTimer; 
-
-		int playState;
-
-        int width, height;
-
-        int maxIndex;
-
-        int currentIndex;
-
-        bool currentAssetIsMovie;
-
-		bool presenting;
-
-		bool showDemo;
-
-
-
-        // GL / GPU
-
-        ofShader shader;
-
-        ofFbo maskFbo;
-
-        ofFbo fbo;
-
-        int currentBrightness;
-
-		int fboAge; 
-
-		int targetAlpha;
-
-
-
-        // Painting
-
-        ofImage brush;
-
-        bool bBrushDown;
-
-
-
-        // Assets
-
-        ofxXmlSettings assets;
-
-        ofTrueTypeFont font;
-
-        vector<string> artistNames;
-
-        vector<string> artistMedia;
+	// Assets
+	ofxXmlSettings assets;
+	ofTrueTypeFont font;
+	vector<string> artistNames;
+	vector<string> artistMedia;
 
 
 };
