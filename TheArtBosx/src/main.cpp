74d06d7a99560257d9fd60b530536b9a0c8553dc
JesseScott <jessecolinscott@gmail.com>
2015-05-24 15:20:02 -0700

    DEBUG macro in main.cpp

--- TheArtBosx/src/main.cpp
+++ TheArtBosx/src/main.cpp
@@ -1,7 +1,13 @@
 #include "ofApp.h"
 
+#define DEBUG
+
 int main() {
-    ofSetupOpenGL(3240, 1920, OF_WINDOW);
+    #ifdef DEBUG
+        ofSetupOpenGL(1920, 1080, OF_WINDOW);
+    #else
+        ofSetupOpenGL(3240, 1920, OF_WINDOW);
+    #endif
 	ofSetWindowPosition(0, 0);
 	ofRunApp(new ofApp());
 }
