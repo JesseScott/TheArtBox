1beb037e073b09977a9bf1581aa361dac36a3946
JesseScott <jessecolinscott@gmail.com>
2016-04-17 18:19:22 -0700

    organized startup batch scripts

---
+++ tabTools/program_start - WEB.bat
@@ -0,0 +1,3 @@
+@echo on
+PING 127.0.0.1 -n 10 >nul
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir=c:\temp http://theartbox.ca/box/index.html --new-window "%1" --window-position=0,0 --window-size=5760,2000
