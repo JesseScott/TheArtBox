2c06484e96108ad719de77e652c4740a9f1614b1
JesseScott <jessecolinscott@gmail.com>
2016-06-27 19:01:37 -0700

    added echo date to startup script

--- tabTools/program_start.bat
+++ tabTools/program_start.bat
@@ -1,3 +1,9 @@
+@echo off
+
+for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
+set mytime=%time%
+echo Current time is %mydate%:%mytime%
+
 @echo on
 PING 127.0.0.1 -n 10 >nul
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir=c:\temp --app="http://theartbox.ca/box/index.html" --new-window "%1" --window-position=0,0 --window-size=5760,2000
